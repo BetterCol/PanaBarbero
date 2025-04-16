917c1c5f3f5df42df6638e1eb81335ce06457d2d
pulgueta <retardix456@gmail.com>
2025-04-15 21:42:59 -0500

    chore: remove unused projects and rename to actual project

--- packages/design-system/components/ui/pagination.tsx
+++ packages/design-system/components/ui/pagination.tsx
@@ -5,8 +5,8 @@ import {
   MoreHorizontalIcon,
 } from "lucide-react"
 
-import { cn } from "@repo/design-system/lib/utils"
-import { Button, buttonVariants } from "@repo/design-system/components/ui/button"
+import { cn } from "@pbarb/design-system/lib/utils"
+import { Button, buttonVariants } from "@pbarb/design-system/components/ui/button"
 
 function Pagination({ className, ...props }: React.ComponentProps<"nav">) {
   return (
--- packages/design-system/index.tsx
+++ packages/design-system/index.tsx
@@ -1,5 +1,5 @@
-import { AnalyticsProvider } from '@repo/analytics';
-import { AuthProvider } from '@repo/auth/provider';
+import { AnalyticsProvider } from '@pbarb/analytics';
+import { AuthProvider } from '@pbarb/auth/provider';
 import type { ThemeProviderProps } from 'next-themes';
 import { Toaster } from './components/ui/sonner';
 import { TooltipProvider } from './components/ui/tooltip';
